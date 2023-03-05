aa7568905922fb1a6847e940c3154b22d8ce35f1
behnam1369 <behnam.aghaali@yahoo.com>
2023-03-05 16:13:22 +0330

    Add screenshot feature

--- src/components/RacingBars.js
+++ src/components/RacingBars.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useMemo } from "react";
 import style from "./RacingBars.module.css";
 import PropTypes from "prop-types";
 import Bar from "./Bar";
+import * as htmlToImage from "html-to-image";
 
 const RacingBars = (props) => {
   const {
@@ -25,17 +26,17 @@ const RacingBars = (props) => {
     [data, title]
   );
   const [playing, setPlaying] = useState(false);
-  // const [timeIndex, setTimeIndex] = useState(-2);
   const [timeIndex, setTimeIndex] = useState(-2);
   const maxScore = useMemo(
     () => Math.max(...data.map((el) => el[value])),
     [data, value]
   );
 
+  const [recording, setRecording] = useState(false);
+
   useEffect(() => {
     if (!playing) return;
     const timeout = setInterval(() => {
-      console.log("timeout");
       setTimeIndex(
         timeIndex >= timePoints.length - 2
           ? timePoints.length - 2
@@ -51,6 +52,10 @@ const RacingBars = (props) => {
   const handleNext = (e) => {
     e.preventDefault();
     setTimeIndex(timeIndex + 1);
+  };
+  const handlePrev = (e) => {
+    e.preventDefault();
+    setTimeIndex(timeIndex - 1);
   };
 
   const rank = (_title, _timeIndex) => {
@@ -75,10 +80,51 @@ const RacingBars = (props) => {
     setPlaying(!playing);
   };
 
+  const handleReset = (e) => {
+    e.preventDefault();
+    setTimeIndex(-2);
+    setPlaying(false);
+  };
+
+  const startRecording = () => {
+    setRecording(true);
+  };
+
+  const stopRecording = () => {
+    setRecording(false);
+  };
+
+  const handleRecord = () => {
+    if (!recording) {
+      setPlaying(true);
+      startRecording();
+    } else {
+      setPlaying(false);
+      stopRecording();
+    }
+  };
+
+  const handleScreenshot = () => {
+    htmlToImage
+      .toJpeg(document.getElementById("racing-bars"), { quality: 1 })
+      .then(function (dataUrl) {
+        var link = document.createElement("a");
+        link.download = "screenshot1.jpeg";
+        link.href = dataUrl;
+        // console.log(link.href);
+        link.click();
+      });
+  };
+
   return (
     <div className={style.main}>
       <h3>Match Day {timePoints[timeIndex + 1]}</h3>
-      <div className={style.chart}>
+
+      <div
+        id="racing-bars"
+        className={style.chart}
+        style={{ height: barHeight * titles.length + 15 + "px" }}
+      >
         {titles.map((title) => (
           <Bar
             key={title}
@@ -110,10 +156,17 @@ const RacingBars = (props) => {
           </Bar>
         ))}
       </div>
+
+      <button onClick={(e) => handlePrev(e)}>Prev</button>
       <button onClick={(e) => handleNext(e)}>Next</button>
       <button onClick={(e) => togglePlay(e)}>
         {playing ? "Pause" : "Play"}
       </button>
+      <button onClick={(e) => handleReset(e)}>Reset</button>
+      {/* <button onClick={(e) => handleRecord(e)}>
+        {recording ? "Stop" : "Record MP4"}
+      </button> */}
+      <button onClick={(e) => handleScreenshot(e)}>Screenshot</button>
     </div>
   );
 };
